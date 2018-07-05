f6c02aadb7e0d9b6dd79b0bccfdcaeef0a13d36e
Sam Morrison <sorrison@gmail.com>
2018-07-05 11:58:40 +1000

    Add resources module
    Change-Id: Iff109b49900140d04870cd6bc818ee0f2dd5f657

---
+++ nectarallocationclient/tests/unit/v1/test_resources.py
@@ -0,0 +1,38 @@
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+from nectarallocationclient import exceptions
+from nectarallocationclient.v1 import resources
+
+from nectarallocationclient.tests.unit import utils
+from nectarallocationclient.tests.unit.v1 import fakes
+
+
+class ResourcesTest(utils.TestCase):
+
+    def setUp(self):
+        super(ResourcesTest, self).setUp()
+        self.cs = fakes.FakeClient()
+
+    def test_allocation_list(self):
+        al = self.cs.resources.list()
+        self.cs.assert_called('GET', '/resources/')
+        for a in al:
+            self.assertIsInstance(a, resources.Resource)
+        self.assertEqual(3, len(al))
+
+    def test_allocation_get(self):
+        a = self.cs.resources.get(1)
+        self.cs.assert_called('GET', '/resources/1/')
+        self.assertIsInstance(a, resources.Resource)
+        self.assertEqual(1, a.id)
