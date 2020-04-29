cb01862f49760946239889326e321d28873389fc
Sam Morrison <sorrison@gmail.com>
2020-04-29 11:26:53 +1000

    Use pbr for parsing requirments
    Change-Id: I5ee01b47318a38fb57e7987f370803732699fa3a

--- setup.py
+++ setup.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
+import setuptools
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from pbr.packaging import parse_requirements
 
-try: # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError: # for pip <= 9.0.3
-    from pip.req import parse_requirements
-
-requirements = parse_requirements("requirements.txt", session=False)
 
 entry_points = {
     'openstack.cli.extension':
@@ -44,9 +35,9 @@ entry_points = {
 }
 
 
-setup(
+setuptools.setup(
     name='nectarallocationclient',
-    version='1.1.0',
+    version='1.2.0',
     description=('Client for the Nectar Allocation system'),
     author='Sam Morrison',
     author_email='sorrison@gmail.com',
@@ -55,7 +46,7 @@ setup(
         'nectarallocationclient',
     ],
     include_package_data=True,
-    install_requires=[str(r.req) for r in requirements],
+    install_requires=parse_requirements(),
     license="Apache",
     zip_safe=False,
     classifiers=(
